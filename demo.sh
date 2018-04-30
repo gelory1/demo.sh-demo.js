0cae56eca9f45c80ea556f505041593b4135e263
gelory <917589697@qq.com>
2018-04-30 17:32:27 +0800

    第二次上传

--- demo.sh
+++ demo.sh
@@ -6,11 +6,11 @@ else
   cd $1
   mkdir css js
   echo "<!DOCTYPE>
-        <title>Hello</title>
-        <h1>Hi</h1>" > index.html
+  <title>Hello</title>
+  <h1>Hi</h1>" > index.html
   echo "h1{color: red;}" > css/style.css
   echo 'var string = "Hello World"
-        alert(string)' > js/main.js
+  alert(string)' > js/main.js
   echo 'success'
   exit
 fi
